4f018760bfc374c1bbb94d831af8f5e9a0a9b96f
Akira Ueda <ue@gol.com>
2020-04-21 18:30:53 +0900

    Add antd styles

--- src/components/IconWithTooltip/index.tsx
+++ src/components/IconWithTooltip/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import Icon, { IconName } from '../Icon';
 import { Tooltip } from 'antd';
+import 'antd/es/tooltip/style/css';
 
 // The inner <span> is a workaround for "Function components cannot be given refs." error in the console.
 // With this error, <Tooltip> does not work.
