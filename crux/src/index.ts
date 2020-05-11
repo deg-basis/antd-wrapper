43d5eb1afc6bb52e9dc1761ff66067c37ba71ef0
Akira Ueda <ue@gol.com>
2020-05-11 10:09:28 +0900

    Export <Upload> and <IconWithTooltip>

--- crux/src/index.ts
+++ crux/src/index.ts
@@ -1,4 +1,6 @@
+export { default as Button } from './components/Button';
 export { default as Heading } from './components/Heading';
 export { default as Icon } from './components/Icon';
 export { default as IconWithLabel } from './components/IconWithLabel';
-export { default as Button } from './components/Button';
+export { default as IconWithTooltip } from './components/IconWithTooltip';
+export { default as Upload } from './components/Upload';
